894e14db54b20f9c01696240534739f151e8cdbe
egorpetrunev <71026996+egorpetrunev@users.noreply.github.com>
2020-09-10 13:35:46 +0300

    tab correction [Ball]

--- Ball.java
+++ Ball.java
@@ -1,2 +1,39 @@
 package com.company;
 
+import java.lang.*;
+
+public class Ball {
+    private String color;
+    private int size;
+
+    public Ball(String color, int size) {
+        this.color = color;
+        this.size = size;
+    }
+
+    public Ball(String color) {
+        this.color = color;
+        this.size = -1;
+    }
+
+    public void setName(String color) {
+        this.color = color;
+    }
+
+    public void setAge(int size) {
+        this.size = size;
+    }
+
+    public String getName() {
+        return this.color;
+    }
+
+    public int getAge() {
+        return this.size;
+    }
+
+    public String toString() {
+        return String.format("color: %s | size: %d", this.color, this.size);
+    }
+
+}
