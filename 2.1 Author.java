6f3148a3a4a78f088037b3be2e4a7e13ce313132
egorpetrunev <71026996+egorpetrunev@users.noreply.github.com>
2020-09-18 13:45:03 +0300

    task 2 I'll create a folder in the evening

---
+++ 2.1 Author.java
@@ -0,0 +1,36 @@
+package com.test;
+
+public class Author {
+    private final String name;
+    private String email;
+    private final char gender;
+
+    public Author(String name, String email, char gender) {
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public char getGender() {
+        return this.gender;
+    }
+
+    public String toString() {
+        return String.format("Name: %s | Email: %s | Gender: %c",
+                this.name,
+                this.email,
+                this.gender);
+    }
+}
